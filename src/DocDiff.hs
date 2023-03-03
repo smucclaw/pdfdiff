c07ad611e9db4a9d1472586a5bc7d282d766bbcf
Wong Meng Weng <mwwong@smu.edu.sg>
2023-03-04 03:17:30 +0800

    print matrix of similarity by levenshtein distance, and dump original text

--- src/DocDiff.hs
+++ src/DocDiff.hs
@@ -5,15 +5,17 @@ module DocDiff where
 import qualified Data.Map as Map
 import qualified Data.Text      as T
 import qualified Data.Text.IO   as TIO
-import Data.Maybe (fromMaybe, listToMaybe)
+import Data.Maybe (fromMaybe, listToMaybe, isJust)
 import Data.List (isSubsequenceOf, sort, sortOn)
 
-import Text.Megaparsec.Char   (string, digitChar, eol, newline, char)
+import Text.Megaparsec.Char as TMC  (string, digitChar, eol, newline, char)
 import Text.Megaparsec
 import Data.Void
-import Control.Monad (void)
+import Control.Monad (void, when)
 
 import Data.Text.Metrics
+
+import Text.PrettyPrint.Boxes
 
 type Filename = String
 type H1       = (Maybe T.Text, T.Text)
@@ -42,8 +44,8 @@ preamble :: Filename -> Parser T.Text
 preamble fn = T.pack <$> manyTill anyChar (lookAhead $ pChunk fn)
 
 dotDigitChar :: Parser Char
-dotDigitChar = digitChar <|> char '.'
-           
+dotDigitChar = digitChar <|> TMC.char '.'
+
 pChunk :: Filename -> Parser (H1, T.Text)
 pChunk fn = do
   let h1 = pH1 fn
@@ -64,7 +66,7 @@ pChunk fn = do
           h1starL = "**Article " *> some dotDigitChar <* ": "
           h1starR = manyTill anyChar ("**" >> eol)
           h1equalsL = "Article " *> some dotDigitChar <* ": "
-          h1equalsR = manyTill anyChar eol <* manyTill (char '=') eol
+          h1equalsR = manyTill anyChar eol <* manyTill (TMC.char '=') eol
       choice [ try $ (,) <$> h1L       <*> h1R
              , try $ (,) <$> h1starL   <*> h1starR
              , try $ (,) <$> h1equalsL <*> h1equalsR ]
@@ -80,34 +82,63 @@ normalize ts = ts
 -- | compute approximate "hash" by calculating the word distribution signature of a given chunk
 
 stats :: FileChunks -> IO ()
-stats fchunks =
-  putStrLn $ unlines
-  [ "* " ++ filename ++ "\n" ++ unlines
-    [ "** " ++ maybe "" T.unpack n ++ " " ++ T.unpack h1 ++
-      "  length:" ++ show (T.length body) ++
-      "  most similar: " ++ maybe "none" shortname
-      (listToMaybe
-        (bySimilarity (fchunks `sans` filename) body))
-      -- ++ "\n" ++ T.unpack body
-    | ((n,h1), body) <- sortOn fstfst (Map.toList filebody) ]
-  | (filename, filebody) <- Map.toList fchunks
-  ]
-  where fstfst ((mt,_),_) = fmap ((read :: (String -> Int)) . T.unpack) . T.splitOn "." <$> mt
-        sans = flip Map.delete
+stats fchunks = do
+  sequence_ [ drawMatrix doc1 doc2
+            | doc1@(fn1,fb1) <- Map.toList   fchunks
+            , doc2@(fn2,fb2) <- Map.toList $ fchunks `sans` fn1
+            ]
+  when True $
+   putStrLn $ unlines
+    [ "* " ++ filename ++ "\n" ++ unlines
+      [ "** " ++ maybe "" T.unpack n ++ " " ++ T.unpack h1 ++
+        "  length:" ++ show (T.length body) ++
+        "  most similar: " ++ maybe "none" shortname
+        (listToMaybe
+         (bySimilarity (fchunks `sans` filename) body))
+        ++ "\n" ++ T.unpack body
+      | ((n,h1), body) <- sortByArtNum (Map.toList filebody)
+      ]
+    | (filename, filebody) <- Map.toList fchunks
+    ]
+  where sans = flip Map.delete
 
+sortByArtNum :: [((Maybe T.Text, b1), b2)] -> [((Maybe T.Text, b1), b2)]
+sortByArtNum = sortOn fstfst
+  where
+    fstfst ((mt,_),_) = fmap ((read :: (String -> Int)) . T.unpack) . T.splitOn "." <$> mt
+        
 shortname :: (Filename, H1) -> String
 shortname (fn, (artnum,h1text)) = sn fn ++ ":" ++ maybe "?" T.unpack artnum ++ "(" ++ T.unpack h1text ++ ")"
-  where sn fn
-          | "SAFTA"   `isSubsequenceOf` fn = "SAFTA"
-          | "ANZSCEP" `isSubsequenceOf` fn = "ANZSCEP"
-          | otherwise                      = fn
+
+sn :: Filename -> String
+sn fn
+  | "SAFTA"   `isSubsequenceOf` fn = "SAFTA"
+  | "ANZSCEP" `isSubsequenceOf` fn = "ANZSCEP"
+  | otherwise                      = fn
 
 bySimilarity :: FileChunks -> T.Text -> [(Filename, H1)]
 bySimilarity fchunks myt =
-  snd <$> (reverse . sort)
+  snd <$> sort
   [ (metric, (fn, h1))
   | (fn, farticles) <- Map.toList fchunks
   , (h1, artbody)   <- Map.toList farticles
-  , let metric = jaroWinkler myt artbody
+  , let metric = levenshtein myt artbody
   ]
   
+
+drawMatrix :: (Filename, Map.Map H1 T.Text) -> (Filename, Map.Map  H1 T.Text) -> IO ()
+drawMatrix doc1@(fn1,fb1) doc2@(fn2,fb2) = do
+  putStrLn $ unwords [ "* matrix:", sn fn1 ++ ends fb1
+                     , "/" ,        sn fn2 ++ ends fb2 ]
+  printBox (hsep 1 left $
+            vcat left (text . T.unpack <$> ("" : headers fb2)) :
+            [ vcat right (text (T.unpack artnum1) :
+                          [ text $ show $ levenshtein artbody1 artbody2
+                          | ((Just artnum2, arttitle2), artbody2) <- sortByArtNum $ Map.toList fb2
+                          ])
+            | ((Just artnum1, arttitle1), artbody1) <- sortByArtNum $ Map.toList fb1
+            ]
+           )
+  where
+    headers fb = [ artnum | ((Just artnum, arttitle), artbody) <- sortByArtNum $ Map.toList fb ]
+    ends fb = "(" ++ (T.unpack . head) (headers fb) ++ "--" ++ (T.unpack . last) (headers fb) ++ ")"
