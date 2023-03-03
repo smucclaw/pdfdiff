920662c604f65623f1584aac4d13550d85ac9ae5
Wong Meng Weng <mwwong@smu.edu.sg>
2023-03-03 16:52:28 +0800

    improve parser to handle ANZSCEP case

--- src/DocDiff.hs
+++ src/DocDiff.hs
@@ -5,14 +5,16 @@ module DocDiff where
 import qualified Data.Map as Map
 import qualified Data.Text      as T
 import qualified Data.Text.IO   as TIO
-import Data.List (isSubsequenceOf, sort)
+import Data.Maybe (fromMaybe)
+import Data.List (isSubsequenceOf, sort, sortOn)
 
 import Text.Megaparsec.Char   (string, digitChar, eol, newline, char)
 import Text.Megaparsec
 import Data.Void
+import Control.Monad (void)
 
 type Filename = String
-type H1       = (Maybe Int, T.Text)
+type H1       = (Maybe T.Text, T.Text)
 type FileChunks = Map.Map Filename (Map.Map H1 T.Text)
 
 preprocess :: [String] -> IO FileChunks
@@ -22,17 +24,17 @@ preprocess filenames = do
   where
     readfile fn = do
       content <- TIO.readFile fn
-      return $ [ ( fn
-                 , Map.fromList $ filechunks fn content )
-                ]
-
+      putStrLn $ fn ++ ": read " ++ show (T.length content) ++ " bytes"
+      return [ ( fn
+               , Map.fromList $ filechunks fn content )
+             ]
 
 -- | we could use a complete markdown parser here, but let's keep it simple until we need one.
 filechunks :: Filename -> T.Text -> [(H1, T.Text)]
 filechunks fn content =
-  case runParser ((,) <$> preamble fn <*> some (pChunk fn)) fn content of
-    Left  x      -> fail $ errorBundlePretty x
-    Right (p,xs) -> ((Just 0, "__PREAMBLE"), p) : xs
+  case runParser ((,) <$> preamble fn <*> some (pChunk fn) <* eof) fn content of
+    Left  x      -> error $ errorBundlePretty x
+    Right (p ,xs) -> ((Nothing, "__PREAMBLE"), p) : xs
 
 preamble :: Filename -> Parser T.Text
 preamble fn = T.pack <$> manyTill anyChar (lookAhead $ pChunk fn)
@@ -41,24 +43,31 @@ dotDigitChar :: Parser Char
 dotDigitChar = digitChar <|> char '.'
            
 pChunk :: Filename -> Parser (H1, T.Text)
-pChunk fn
-  | "SAFTA" `isSubsequenceOf` fn = do
-      let articleN :: Parser String
-          articleN = "ARTICLE " *> some digitChar <* many eol
-          pH1 :: Parser String
-          pH1 = manyTill anyChar eol <* many "="
-      aN <- articleN
-      h1 <- pH1
-      tx <- (T.pack <$> (manyTill anyChar (eof <|> () <$ lookAhead articleN)))
-      return ((Just (read aN), T.pack h1), tx)
-  | "ANZSCEP" `isSubsequenceOf` fn = do
-      let h1 = (,)
-               <$> ("**Article " *> (Just . read <$> some dotDigitChar) <* ": ")
-               <*> (T.pack <$> manyTill anyChar (eof <|> (() <$ ("**" >> eol))))
-      (,)
-        <$> h1
-        <*> (T.pack <$> manyTill anyChar (lookAhead h1))
+pChunk fn = do
+  let h1 = pH1 fn
+  (,)
+    <$> (mapPair (Just . T.pack, T.pack) <$> h1)
+    <*> (T.pack <$> manyTill anyChar (eof <|> void (lookAhead h1)))
+  where
+    mapPair (f1,f2) (x,y) = (f1 x, f2 y)
+    pH1 fn
+      | "SAFTA" `isSubsequenceOf` fn = do
+      let articleN = "ARTICLE " *> some dotDigitChar <* many eol
+          pH1 = manyTill anyChar eol <* many "=" <* many eol
+      (,) <$> articleN <*> pH1
 
+      | "ANZSCEP" `isSubsequenceOf` fn = do
+      let h1L = T.unpack <$> string "111"
+          h1R = T.unpack <$> string "bar"
+          h1starL = "**Article " *> some dotDigitChar <* ": "
+          h1starR = manyTill anyChar ("**" >> eol)
+          h1equalsL = "Article " *> some dotDigitChar <* ": "
+          h1equalsR = manyTill anyChar eol <* manyTill (char '=') eol
+      choice [ try $ (,) <$> h1L       <*> h1R
+             , try $ (,) <$> h1starL   <*> h1starR
+             , try $ (,) <$> h1equalsL <*> h1equalsR ]
+
+      | otherwise = error ("not prepared to process file " ++ fn)
 anyChar = satisfy (const True)
 
 type Parser = Parsec Void T.Text
@@ -71,7 +80,10 @@ normalize ts = ts
 stats :: FileChunks -> IO ()
 stats fchunks =
   putStrLn $ unlines [ "* " ++ filename ++ "\n" ++
-                       unlines [ "** " ++ maybe "" show n ++ " " ++ T.unpack h1
-                               | ((n,h1), body) <- sort (Map.toList filebody) ]
+                       unlines [ "** " ++ maybe "" T.unpack n ++ " " ++ T.unpack h1 ++
+                                 "  length:" ++ show (T.length body)
+                                 -- ++ "\n" ++ T.unpack body
+                               | ((n,h1), body) <- sortOn fstfst (Map.toList filebody) ]
                      | (filename, filebody) <- Map.toList fchunks
                      ]
+  where fstfst ((mt,_),_) = fmap ((read :: (String -> Int)) . T.unpack) . T.splitOn "." <$> mt
