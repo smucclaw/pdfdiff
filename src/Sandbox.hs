9fc9f85c71c4fe80fa4a6cbcf9524ca75a047424
Inari Listenmaa <inari.listenmaa@gmail.com>
2023-04-05 21:33:56 +0800

    use a rose tree instead of nested lists of tuples

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -27,22 +27,39 @@ sc = L.space (void $ some (char ' ' <|> char '\t')) lineComment empty
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
-pItem :: Parser String
+pItem :: Parser Item
 pItem = lexeme (some anyChar) <?> "list item"
 
-pComplexItem :: Parser (String, [String])
+-----------------------------------------------------------------------------
+-- Custom data types
+
+type Item = String
+data ItemList = IL {top :: Item, args :: [ItemList]}  deriving (Show,Eq)
+
+item2list :: Item -> ItemList
+item2list x = IL x []
+
+items2IL :: [Item] -> [ItemList]
+items2IL xs = [IL x [] | x <- xs]
+
+-----------------------------------------------------------------------------
+-- The parser
+
+--  Parser (String, [String])
+pComplexItem :: Parser ItemList
 pComplexItem = L.indentBlock scn p
   where
     p = do
       header <- pItem
-      return (L.IndentMany Nothing (return . (header, )) pItem)
+      return (L.IndentMany Nothing (pure . IL header . items2IL) pItem)
 
-pItemList :: Parser (String, [(String, [String])])
+pItemList :: Parser ItemList
 pItemList = L.nonIndented scn (L.indentBlock scn p)
   where
     p = do
       header <- pItem
-      return (L.IndentMany Nothing (return . (header, )) pComplexItem)
+      return (L.IndentMany Nothing (return . IL header) pComplexItem)
+
 
 -----------------------------------------------------------------------------
 -- stuff to try out in the command line
@@ -51,10 +68,12 @@ test = parseTest (some pItemList <* eof) testList
 
 testList :: Text
 testList = pack $ unlines [
-    "For the purposes of this Parsing Exercise:",
-    "(a) \"sdfdfgsfgs\" means the *sdgdfgdhf*, set out in Annex 999 to the ASDFKJDFSF Agreement;",
-    "(b) \"dfgkldfhkdlfh\" means, with respect to a sdfksgljgfsd:",
-    "  i.  lkhjhdku; or",
-    "  ii. uiykhchniwgredfbcvmjh dfhljsgh:",
-    "    A.  sdlj dsfgj asjhf kjjgk gasdfjh hdflhjasgh; or",
-    "    B.  lashg ioiehl aslfajföä sdflkgsh"]
+    "For the purposes of this Parsing Exercise:"
+  , "(a) \"sdfdfgsfgs\" means the *sdgdfgdhf*, set out in Annex 999 to the ASDFKJDFSF Agreement;"
+  , "(b) \"dfgkldfhkdlfh\" means, with respect to a sdfksgljgfsd:"
+  , "  i.  lkhjhdku; or"
+  , "  ii. uiykhchniwgredfbcvmjh dfhljsgh:"
+  , "    A.  sdlj dsfgj asjhf kjjgk gasdfjh hdflhjasgh; or"
+  , "    B.  lashg ioiehl aslfajföä sdflkgsh"
+--  , "       1.  ALL THE NESTING WOOOOO"
+    ]
